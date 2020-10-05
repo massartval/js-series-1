e1740b48783f0cdaadf8f1dabd30dc1e25a9cce9
massartval <massart.val@gmail.com>
2020-10-05 11:41:07 +0200

    Completed 01.03

--- 01-base/03-prompt-if/script.js
+++ 01-base/03-prompt-if/script.js
@@ -4,8 +4,15 @@
 (() => {
   // Don't bother with that, look at your browser's console(chrome), you can delete it afterwards ;)
   console.log('%c ', 'padding: 38px; background:url(https://becode.org/app/uploads/2020/03/bc_mailsign_seal.png) no-repeat;');
-  console.log('Hello Woods programmer');
+  console.log('Hello Keller programmer');
 
   // YOUR CODE HERE
+  let cake = prompt (`Do you want more cake? (yes/no)`);
+  if ((cake === 'yes') || (cake === 'Yes')) {
+    alert(`Congratjulashons!`);
+  } 
+  else {
+    alert(`Moar kek for me den!`);
+  } 
 })();
 
