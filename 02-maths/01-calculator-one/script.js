2b734afe78b1d26a79f93b5f3f5c93613d2a22d1
massartval <massart.val@gmail.com>
2020-10-05 15:04:30 +0200

    Completed 02.01

--- 02-maths/01-calculator-one/script.js
+++ 02-maths/01-calculator-one/script.js
@@ -3,20 +3,25 @@
 
 (() => {
     // to get the value of an input: document.getElementById("element-id").value
+    // +string turns a string into a number
 
     document.getElementById("addition").addEventListener("click", function() {
         // perform an addition
+        alert(+document.getElementById("op-one").value + +document.getElementById("op-two").value)
     });
 
     document.getElementById("substraction").addEventListener("click", function() {
         // perform an substraction
+        alert(+document.getElementById("op-one").value - +document.getElementById("op-two").value)
     });
 
     document.getElementById("multiplication").addEventListener("click", function() {
         // perform an multiplication
+        alert(+document.getElementById("op-one").value * +document.getElementById("op-two").value)
     });
 
     document.getElementById("division").addEventListener("click", function() {
         // perform an division
+        alert(+document.getElementById("op-one").value / +document.getElementById("op-two").value)
     });
 })();
