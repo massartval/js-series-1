af5fb01922540ba3117c119134c89a6f0fec7563
massartval <massart.val@gmail.com>
2020-10-05 11:21:14 +0200

    Completed 01.02

--- 01-base/02-var-prompt/script.js
+++ 01-base/02-var-prompt/script.js
@@ -4,8 +4,11 @@
 (() => {
     // Don't bother with that, look at your browser's console(chrome), you can delete it afterwards ;)
     console.log('%c ', 'padding: 38px; background:url(https://becode.org/app/uploads/2020/03/bc_mailsign_seal.png) no-repeat;');
-    console.log('Hello Woods programmer');
+    console.log('Hello Keller programmer');
 
     // YOUR CODE HERE
+
+    let userName = prompt("Please enter your name", "Harry Potter");
+    alert(`Hello, ${userName}`);
 })();
 
