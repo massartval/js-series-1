1c113fd0bf9f77c0e70356b5a3455a1f8506d531
massartval <massart.val@gmail.com>
2020-10-06 11:51:19 +0200

    Completed 02.04

--- 02-maths/04-sort-numbers/script.js
+++ 02-maths/04-sort-numbers/script.js
@@ -1,13 +1,26 @@
 // 02-maths/04-sort-numbers/script.js - 2.4: Sorting the numbers
 
 (() => {
+  // to get the value of an input: document.getElementById("element-id").value
 
-    // to get the value of an input: document.getElementById("element-id").value
-
-    document.getElementById("run").addEventListener("click", () => {
-
-        // your code here
-
-    });
-
+  document.getElementById("run").addEventListener("click", () => {
+    // your code here
+    let numbers = [];
+    /**
+     * Returns the numbers in an array instead of a string
+     */ function stringToArray() {
+      numbers = document.getElementById("numbers").value.split(",");
+      return numbers;
+    }
+    /**
+     * Returns the array sorted in ascending order
+     * @param function(a, b) Compares a with b and returns the difference
+     */ function sort() {
+      numbers.sort(function (a, b) {
+        return a - b;
+      });
+    }
+    sort(stringToArray());
+    alert(numbers);
+  });
 })();
