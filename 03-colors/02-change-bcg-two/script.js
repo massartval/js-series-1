4d75b58336cd6f8bb47f191967fcd8c52cf4e18e
massartval <massart.val@gmail.com>
2020-10-06 15:49:50 +0200

    Completed 03.02

--- 03-colors/02-change-bcg-two/script.js
+++ 03-colors/02-change-bcg-two/script.js
@@ -1,8 +1,13 @@
-
 // 03-colors/02-change-bcg-two/script.js - 3.2: Bcg 2
 
 (() => {
-
-    // your code here
-
+  // your code here
+  document.getElementById("run").addEventListener("click", function () {
+    function changeBgColor() {
+      document.body.style.backgroundColor = document.getElementById(
+        "color"
+      ).value;
+    }
+    changeBgColor();
+  });
 })();
