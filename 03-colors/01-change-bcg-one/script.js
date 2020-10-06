9196caac45dd8f90c95b7ff34df06cf13f689c7b
massartval <massart.val@gmail.com>
2020-10-06 15:44:36 +0200

    Completed 03.01

--- 03-colors/01-change-bcg-one/script.js
+++ 03-colors/01-change-bcg-one/script.js
@@ -1,9 +1,29 @@
-
 // 03-colors/01-change-bcg-one/script.js - 3.1: Bcg one (1)
 
-
 (() => {
-
-    // your code here
-
+  // your code here
+  document.getElementById("red").addEventListener("click", function () {
+    function changeBgColorToRed() {
+      document.body.style.backgroundColor = "red";
+    }
+    changeBgColorToRed();
+  });
+  document.getElementById("green").addEventListener("click", function () {
+    function changeBgColorToRed() {
+      document.body.style.backgroundColor = "green";
+    }
+    changeBgColorToRed();
+  });
+  document.getElementById("yellow").addEventListener("click", function () {
+    function changeBgColorToRed() {
+      document.body.style.backgroundColor = "yellow";
+    }
+    changeBgColorToRed();
+  });
+  document.getElementById("blue").addEventListener("click", function () {
+    function changeBgColorToRed() {
+      document.body.style.backgroundColor = "blue";
+    }
+    changeBgColorToRed();
+  });
 })();
