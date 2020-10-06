ed9c476c85faed030676e60c6e297430514e9c90
massartval <massart.val@gmail.com>
2020-10-06 12:11:40 +0200

    Completed 02.05

--- 02-maths/05-factorial/script.js
+++ 02-maths/05-factorial/script.js
@@ -1,14 +1,27 @@
-
 // 02-maths/05-factorial/script.js - 2.5: Factorial
 
 (() => {
-    
-    // to get the value of an input: document.getElementById("element-id").value
+  // to get the value of an input: document.getElementById("element-id").value
 
-    document.getElementById("run").addEventListener("click", () => {
-
-        // your code here
-
-    });
-
+  document.getElementById("run").addEventListener("click", () => {
+    // your code here
+    let n = Number(document.getElementById("number").value);
+    /**
+     * Returns the factorial of a given number
+     * @param {number} n The number to factor
+     */
+    function factorial(n) {
+      let acc = 1;
+      if (n === 0) {
+        return acc;
+      } else if (n > 0) {
+        acc = n * factorial(n - 1);
+        return acc;
+      } else {
+        acc = "impossible to calculate";
+        return acc;
+      }
+    }
+    alert(`The factorial of ${n} is ${factorial(n)}`);
+  });
 })();
