ebed43ce2552443cb517757fec56e41122453c4c
massartval <massart.val@gmail.com>
2020-10-07 16:02:39 +0200

    Completed 04.01

--- 04-dates/01-title-by-hour-one/script.js
+++ 04-dates/01-title-by-hour-one/script.js
@@ -1,11 +1,25 @@
-
 // 04-dates/01-title-by-hour-one/script.js - 4.1: text according to the hour (1)
 
-
 (() => {
+  // to change the content of a tag: document.getElementById("element-id").innerHTML = "new-value"
 
-    // to change the content of a tag: document.getElementById("element-id").innerHTML = "new-value"
-
-    // your code here
-
+  // your code here
+  /**
+   * Returns local hour between 0 and 23
+   */ function getTime() {
+    let date = new Date();
+    let hour = date.getHours();
+    document.getElementById("target").innerHTML = hour;
+    return hour;
+  }
+  /**
+   * Returns the correct greeting according to the time of the day
+   */ function greetings() {
+    if (getTime() < 18) {
+      document.getElementById("target").innerHTML = "Hello!";
+    } else {
+      document.getElementById("target").innerHTML = "Good evening!";
+    }
+  }
+  greetings();
 })();
