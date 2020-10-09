71aa6a5c62bb6f928c631c56754c682b6f4c4d55
massartval <massart.val@gmail.com>
2020-10-09 12:35:48 +0200

    Completed 05.02

--- 05-arrays/02-manipulate-array/script.js
+++ 05-arrays/02-manipulate-array/script.js
@@ -1,20 +1,16 @@
 // 05-arrays/02-manipulate-array/script.js - 5.2: manipulate array
 
 (() => {
+  let fruits = ["pomme", "poire", "fraise", "tomate", "orange", "mandarine", "durian", "pêche", "raisin", "cerise"];
 
-    let fruits = [
-        "pomme",
-        "poire",
-        "fraise",
-        "tomate",
-        "orange",
-        "mandarine",
-        "durian",
-        "pêche",
-        "raisin",
-        "cerise",
-    ];
-
-    // your code here
-
+  // your code here
+  document.getElementById("run").addEventListener("click", function () {
+    console.log(fruits);
+    // Starts modifying the array at index 0, removes 1 item and adds "banana"
+    fruits.splice(0, 1, "banana");
+    console.log(fruits);
+    // Starts modifying the array at the end and removes 1 item
+    fruits.splice(fruits.length - 1, 1, "kiwi");
+    console.log(fruits);
+  });
 })();
