cf6a6b2805bc13d36582474871f14d732cb58dac
massartval <massart.val@gmail.com>
2020-10-09 10:50:06 +0200

    Completed 07.01

--- 07-classes/01-instances/script.js
+++ 07-classes/01-instances/script.js
@@ -1,12 +1,17 @@
 // 07-classes/01-instances/script.js - 7.1: instances
 
-
 (() => {
-    class Cat {
-        constructor(name, age) {
-            this.name = name;
-            this.age = age;
-        }
+  class Cat {
+    constructor(name, age) {
+      this.name = name;
+      this.age = age;
     }
-    // your code here
+  }
+  // your code here
+  document.getElementById("run").addEventListener("click", function () {
+    let Skitty = new Cat("Skitty", "9");
+    let Pixel = new Cat("Pixel", "6");
+    console.log(Skitty);
+    console.log(Pixel);
+  });
 })();
