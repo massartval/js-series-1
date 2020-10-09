0cc686fedbc1f9888a224fd2730eefe04f41f92b
massartval <massart.val@gmail.com>
2020-10-09 13:54:53 +0200

    Updated 06.01

--- 06-objects/01-generate-object/script.js
+++ 06-objects/01-generate-object/script.js
@@ -5,5 +5,6 @@
   document.getElementById("run").addEventListener("click", function () {
     let me = { lastName: "Massart", firstName: "Valentin", age: 28, city: "Charleroi", country: "Belgium" };
     console.log(me);
+    alert(`Salut, je m'appelle ${me.firstName} ${me.lastName}, I am ${me.age} and I live in ${me.city}, ${me.country}.`);
   });
 })();
