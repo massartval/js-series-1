c1ef5700ce530b4aaa9a7ed78b2f6a5c87a543c5
massartval <massart.val@gmail.com>
2020-10-09 12:08:49 +0200

    Started 10.01 + created notes.md

--- 10-asynchronous/01-get-posts/script.js
+++ 10-asynchronous/01-get-posts/script.js
@@ -1,6 +1,15 @@
 // 10-asynchronous/01-get-posts/script.js - 10.1: chargement d'articles
 
-
 (() => {
-    // your code here
+  // your code here
+  document.getElementById("run").addEventListener("click", () => {
+    function getPosts(callback) {
+      alert(`Open the console to see callback`);
+      callback();
+    }
+
+    getPosts(function () {
+      console.log(getPosts());
+    });
+  });
 })();
---
+++ notes.md
@@ -0,0 +1,4 @@
+10.01 : Not working, not sure why
+https://codeburst.io/javascript-what-the-heck-is-a-callback-aba4da2deced
+https://stackoverflow.com/questions/34034663/node-js-callback-function-is-not-defined
+https://javascript.info/callbacks
