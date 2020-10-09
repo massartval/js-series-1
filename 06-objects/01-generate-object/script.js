6ea0e93c8f6d88bcc074b8dbd9fd7f849672aa0d
massartval <massart.val@gmail.com>
2020-10-09 10:38:51 +0200

    Completed 06.01

--- 06-objects/01-generate-object/script.js
+++ 06-objects/01-generate-object/script.js
@@ -1,6 +1,9 @@
 // 06-objects/01-generate-object/script.js - 6.1: genererate an object
 
-
 (() => {
-    // your code here
+  // your code here
+  document.getElementById("run").addEventListener("click", function () {
+    let me = { lastName: "Massart", firstName: "Valentin", age: 28, city: "Charleroi", country: "Belgium" };
+    console.log(me);
+  });
 })();
