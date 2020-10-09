2b90aedbbed9b19187e154dca2786b72924fe0ea
massartval <massart.val@gmail.com>
2020-10-09 09:46:40 +0200

    Completed 05.01

--- 05-arrays/01-get-element/script.js
+++ 05-arrays/01-get-element/script.js
@@ -1,22 +1,10 @@
 // 05-arrays/01-get-element/script.js - 5.1: item in array
 
 (() => {
+  let fruits = ["apple", "pear", "raspberry", "tomatoes", "kiwi", "banana", "orange", "mandarin", "durian", "peache", "grap", "cherry"];
 
-    let fruits = [
-        "apple",
-        "pear",
-        "raspberry",
-        "tomatos",
-        "kiwi",
-        "banana",
-        "orange",
-        "mandarin",
-        "durian",
-        "peache",
-        "grap",
-        "cherry",
-    ];
-
-    // your code here
-
+  // your code here
+  document.getElementById("run").addEventListener("click", function () {
+    alert(fruits[3]);
+  });
 })();
