6f079fa995d66ba06751cd4a138ae6a84c886fb6
massartval <massart.val@gmail.com>
2020-10-12 16:42:54 +0200

    Completed 05.01

--- 05-arrays/03-walk-one/script.js
+++ 05-arrays/03-walk-one/script.js
@@ -1,23 +1,12 @@
 // 05-arrays/01-get-element/script.js - 5.1: walk through the list
 
-
 (() => {
+  let fruits = ["apple", "pear", "raspberry", "tomatoes", "kiwi", "banana", "orange", "mandarin", "durian", "peache", "grap", "cherry"];
 
-    let fruits = [
-        "apple",
-        "pear",
-        "raspberry",
-        "tomatos",
-        "kiwi",
-        "banana",
-        "orange",
-        "mandarin",
-        "durian",
-        "peache",
-        "grap",
-        "cherry",
-    ];
-
-    // your code here
-
+  // your code here
+  document.getElementById("run").addEventListener("click", function () {
+    for (elem of fruits) {
+      console.log(elem);
+    }
+  });
 })();
