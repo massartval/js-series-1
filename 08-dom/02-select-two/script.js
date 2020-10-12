b0e3247dc3bedfe6d9e52bbe7a2cad8a9d626a1b
massartval <massart.val@gmail.com>
2020-10-12 15:55:38 +0200

    Completed 08.02

--- 08-dom/02-select-two/script.js
+++ 08-dom/02-select-two/script.js
@@ -1,8 +1,8 @@
 // 08-dom/02-select-two/script.js - 8.2: select by css selector
 
-
 (() => {
-
-    // your code here
-
+  // your code here
+  document.querySelector("#target").innerHTML = "owned";
+  // Another solution
+  //document.getElementsByClassName("material")[0].innerHTML = '<p id="target">owned</p>';
 })();
