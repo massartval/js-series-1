6e0e3e59a39bff6979b9f969b27fb7a34d5713b9
massartval <massart.val@gmail.com>
2020-10-12 14:31:35 +0200

    Completed 06.02

--- 06-objects/02-assign-properties/script.js
+++ 06-objects/02-assign-properties/script.js
@@ -1,8 +1,8 @@
 //06-objects/02-assign-properties/script.js - 6.2: assign properties
 
-
 (() => {
-    const computers = [
+  //prettier-ignore
+  const computers = [
         {id: "0001", available: false, user: "leny", os: "macOS"},
         {id: "0002", available: false, user: "Nicolas"},
         {id: "0003"},
@@ -13,10 +13,38 @@
         {id: "0008"},
         {id: "0009", available: false, user: "Anthony"},
     ];
-    const defaultProps = {
-        available: true,
-        os: "linux",
-        user: null,
-    };
-    // your code here
+  const defaultProps = {
+    available: true,
+    os: "linux",
+    user: null,
+  };
+  // your code here
+  document.getElementById("run").addEventListener("click", function () {
+    for (item of computers) {
+      let temp = {};
+      // Adds the properties of defaultProps to temp
+      Object.assign(temp, defaultProps);
+      // Adds the properties of an item of the array computers to temp, overwriting the default properties if needed
+      Object.assign(temp, item);
+      // Adds the properties of temp to item
+      Object.assign(item, temp);
+    }
+    console.log(computers);
+  });
 })();
+
+/**
+ * First attempt : 
+ * 
+ * // Iterate through items in the array computers
+    for (let elem of computers) {
+      // Iterate through the properties of each object
+      for (let property in elem) {
+        // If a property is missing, it should be replaced by the default value
+        if (available && user && os == false) {
+          elem.property = defaultProps.property;
+        }
+      }
+    }
+    console.log(computers);
+ */
--- notes.md
+++ notes.md
@@ -2,3 +2,10 @@
 https://codeburst.io/javascript-what-the-heck-is-a-callback-aba4da2deced
 https://stackoverflow.com/questions/34034663/node-js-callback-function-is-not-defined
 https://javascript.info/callbacks
+
+6.2 : Not working
+https://appdividend.com/2020/07/27/javascript-how-to-add-property-to-object/
+https://dreaminginjavascript.wordpress.com/2008/06/27/how-weird-are-javascript-arrays/
+https://stackoverflow.com/questions/1168807/how-can-i-add-a-key-value-pair-to-a-javascript-object
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/defineProperty
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/defineProperties
