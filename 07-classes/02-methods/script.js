48e6b7cb37f5478f5798cda8f4e368daf1621d90
massartval <massart.val@gmail.com>
2020-10-12 15:30:24 +0200

    Completed 07.02

--- 07-classes/02-methods/script.js
+++ 07-classes/02-methods/script.js
@@ -1,6 +1,18 @@
 //07-classes/02-methods/script.js - 7.2: methods
 
-
 (() => {
-    // your code here
+  // your code here
+  class Person {
+    constructor(firstName, lastName) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+    }
+    sayHello() {
+      return `Hello, ${this.firstName} ${this.lastName}!`;
+    }
+  }
+  document.getElementById("run").addEventListener("click", function () {
+    let Arnold = new Person("Arnold", "Schwarzenegger");
+    console.log(Arnold.sayHello());
+  });
 })();
