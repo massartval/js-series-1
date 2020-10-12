90662d618e3e975170c2362b19b1f8ff4753bd7c
massartval <massart.val@gmail.com>
2020-10-12 16:58:58 +0200

    Completed 05.04

--- 05-arrays/04-walk-two/script.js
+++ 05-arrays/04-walk-two/script.js
@@ -1,23 +1,14 @@
 // 05-arrays/04-walk-two/script.js - 5.4: walk through the list (2)
 
-
 (() => {
+  let fruits = ["pomme", "poire", "fraise", "tomate", "kiwi", "banane", "orange", "mandarine", "durian", "pêche", "raisin", "cerise"];
 
-    let fruits = [
-        "pomme",
-        "poire",
-        "fraise",
-        "tomate",
-        "kiwi",
-        "banane",
-        "orange",
-        "mandarine",
-        "durian",
-        "pêche",
-        "raisin",
-        "cerise",
-    ];
+  // your code here
+  document.getElementById("run").addEventListener("click", function () {
+    fruits.forEach(myFunction);
 
-    // your code here
-
+    function myFunction(item, index) {
+      console.log(`${index} : ${item}`);
+    }
+  });
 })();
