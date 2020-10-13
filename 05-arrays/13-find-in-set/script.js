2d590015695cb48aa872ea21d93bd47c2f449dde
massartval <massart.val@gmail.com>
2020-10-13 17:09:48 +0200

    Completed 05.13

--- 05-arrays/13-find-in-set/script.js
+++ 05-arrays/13-find-in-set/script.js
@@ -1,18 +1,11 @@
 // 05-arrays/13-find-in-set/script.js - 5.13: Find in a Set
 
-
 (() => {
-    const people = new Set([
-        "Nicolas",
-        "Nick",
-        "Leny",
-        "Alexandre",
-        "Charlène",
-        "Laureline",
-        "Esther",
-        "Simon",
-        "Lucas",
-    ]);
+  const people = new Set(["Nicolas", "Nick", "Leny", "Alexandre", "Charlène", "Laureline", "Esther", "Simon", "Lucas"]);
 
-    // your code here
+  // your code here
+  document.getElementById("run").addEventListener("click", function () {
+    console.log(people.size);
+    console.log(people.has("Alexandre"));
+  });
 })();
