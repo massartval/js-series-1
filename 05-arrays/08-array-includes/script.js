4cb5bc4071d3ecf9463b4f2452c359b4e2ca8935
massartval <massart.val@gmail.com>
2020-10-13 15:36:14 +0200

    Completed 05.08

--- 05-arrays/08-array-includes/script.js
+++ 05-arrays/08-array-includes/script.js
@@ -1,22 +1,16 @@
 // 05-arrays/08-array-includes/script.js - 5.8: presence in an aray
 
 (() => {
+  const fruits = ["apple", "perry", "strawberry", "tomato", "kiwi", "banana", "orange", "mandarin", "durian", "peach", "grapes", "cherry"];
 
-    const fruits = [
-        "apple",
-        "perry",
-        "strawberry",
-        "tomato",
-        "kiwi",
-        "banana",
-        "orange",
-        "mandarin",
-        "durian",
-        "peach",
-        "grapes",
-        "cherry",
-    ];
-
-    // your code here
-
+  // your code here
+  document.getElementById("run").addEventListener("click", function () {
+    console.log(fruits);
+    console.log(`Does this array include an apple?`);
+    if (fruits.includes("apple")) {
+      console.log(`Yes, it does.`);
+    } else {
+      console.log(`No, it doesn't.`);
+    }
+  });
 })();
